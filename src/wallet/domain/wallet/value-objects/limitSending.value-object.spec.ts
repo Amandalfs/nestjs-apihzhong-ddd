5725c287ba440f1742d6926fbfe35a94ec7522a1
Amandalfs <amandalfs57@gmail.com>
2023-10-23 13:34:24 -0300

    fix: rename limits by tests

--- src/wallet/domain/wallet/value-objects/limitSending.value-object.spec.ts
+++ src/wallet/domain/wallet/value-objects/limitSending.value-object.spec.ts
@@ -2,20 +2,20 @@ import { LimitSending } from './limitSending.value-object';
 
 describe('limit withdraw tests units', () => {
   it('should possible check limit', () => {
-    const limitSendingByDaily = new LimitSending({
+    const limitSending = new LimitSending({
       limit: 300,
     });
 
-    expect(limitSendingByDaily.checkLimit(200)).toEqual(true);
+    expect(limitSending.checkLimit(200)).toEqual(true);
   });
 
   it('should throw an error if the limit is exceeded.', () => {
-    const limitSendingByDaily = new LimitSending({
+    const limitSending = new LimitSending({
       limit: 300,
     });
 
     expect(() => {
-      limitSendingByDaily.checkLimit(500);
+      limitSending.checkLimit(500);
     }).toThrowError('Impossible to sending this amount at once.');
   });
 });
--- src/wallet/domain/wallet/value-objects/limitWithdraw.value-object.spec.ts
+++ src/wallet/domain/wallet/value-objects/limitWithdraw.value-object.spec.ts
@@ -2,20 +2,20 @@ import { LimitWithdraw } from './limitWithdraw.value-object';
 
 describe('limit withdraw tests units', () => {
   it('should possible check limit', () => {
-    const limitWithdrawByDaily = new LimitWithdraw({
+    const limitWithdraw = new LimitWithdraw({
       limit: 300,
     });
 
-    expect(limitWithdrawByDaily.checkLimit(200)).toEqual(true);
+    expect(limitWithdraw.checkLimit(200)).toEqual(true);
   });
 
   it('should throw an error if the limit is exceeded.', () => {
-    const limitWithdrawByDaily = new LimitWithdraw({
+    const limitWithdraw = new LimitWithdraw({
       limit: 300,
     });
 
     expect(() => {
-      limitWithdrawByDaily.checkLimit(500);
+      limitWithdraw.checkLimit(500);
     }).toThrowError('Impossible to withdraw this amount at once.');
   });
 });
