8fc490eb08a2326f792d6e28941cc1b7b9ab545e
Amandalfs <amandalfs57@gmail.com>
2023-11-08 15:03:51 -0300

    feat: add to use cases the module wallet

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,26 @@
 import { InfraModule } from '@/infra/infra.module';
 import { Module } from '@nestjs/common';
+import { CreateAgencyUseCase } from './usecases/CreateAgencyUseCase/CreateAgencyUseCase';
+import { CreateWalletUseCase } from './usecases/CreateWalletUseCase/CreateWalletUseCase';
+import { DepositBalanceUseCase } from './usecases/DepositBalanceUseCase/DepositBalanceUseCase';
+import { SendingBalanceUseCase } from './usecases/SendingBalanceUseCase/SendingBalanceUseCase';
+import { WithdrawBalanceUseCase } from './usecases/WithdrawBalanceUseCase/WithdrawBalanceUseCase';
 
 @Module({
   imports: [InfraModule],
-  providers: [],
-  exports: [],
+  providers: [
+    CreateAgencyUseCase,
+    CreateWalletUseCase,
+    DepositBalanceUseCase,
+    SendingBalanceUseCase,
+    WithdrawBalanceUseCase,
+  ],
+  exports: [
+    CreateAgencyUseCase,
+    CreateWalletUseCase,
+    DepositBalanceUseCase,
+    SendingBalanceUseCase,
+    WithdrawBalanceUseCase,
+  ],
 })
 export class WalletModule {}
